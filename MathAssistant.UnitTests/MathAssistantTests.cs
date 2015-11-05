dd63a32bcbe9716c4e765a2c36bfcaf11c74a078
Alexander Brown <alexander.brown@a4direct.com>
2015-11-05 23:14:13 +0000

    Add First unit test - failing

---
+++ MathAssistant.UnitTests/MathAssistantTests.cs
@@ -0,0 +1,21 @@
+﻿using MathAssistant.Core;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MathAssistant.UnitTests
+{
+    [TestFixture]
+    public class MathAssistantTests
+    {
+        [Test]
+        public void WhenStart__ShouldBePromptedToEnterSequenceNumber()
+        {
+            var console = MockRepository.GenerateMock<IConsole>();
+            var application = new MathAssistantApplication(console);
+
+            application.Start();
+
+            console.AssertWasCalled(x => x.WriteLine("Enter Sequence Number:"), option => option.Repeat.Once());
+        }
+    }
+}
