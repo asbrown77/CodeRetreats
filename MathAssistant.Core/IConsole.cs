785cb099ab018d6d4f85775ce1fa6d02e7d602df
Alexander Brown <alexander.brown@a4direct.com>
2015-11-05 23:20:55 +0000

    Add second unit test - failing

--- MathAssistant.Core/IConsole.cs
+++ MathAssistant.Core/IConsole.cs
@@ -3,5 +3,6 @@
     public interface IConsole
     {
         void WriteLine(string value);
+        string ReadLine();
     }
 }
--- MathAssistant.UnitTests/MathAssistantTests.cs
+++ MathAssistant.UnitTests/MathAssistantTests.cs
@@ -17,5 +17,16 @@ namespace MathAssistant.UnitTests
 
             console.AssertWasCalled(x => x.WriteLine("Enter Sequence Number:"), option => option.Repeat.Once());
         }
+
+        [Test]
+        public void WhenStart_ShouldWaitForResponse()
+        {
+            var console = MockRepository.GenerateMock<IConsole>();
+            var application = new MathAssistantApplication(console);
+
+            application.Start();
+
+            console.AssertWasCalled(x => x.ReadLine(), options => options.Repeat.Once());
+        }
     }
 }
