4eb74fdbb3b3eeaa84104e7a5f559f4c317abd9d
Alexander Brown <alexander.brown@a4direct.com>
2015-11-05 22:58:02 +0000

    Add MathAssistant Definition step (missing solution file update)

---
+++ MathAssistant.AcceptanceTests/StepDefinitions/MathAssistantSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MathAssistant.AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public class FizzBuzzTransformationSteps
+    {
+        [Given(@"I prompted to enter the total number of sequence numbers")]
+        public void GivenIPromptedToEnterTheTotalNumberOfSequenceNumbers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I have entered in ""(.*)""")]
+        public void WhenIHaveEnteredIn(string inputValue)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"should exit application")]
+        public void ThenShouldExitApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
