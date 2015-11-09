68afd5cfbb3aa204fb6822345a244839e175bfeb
Alexander Brown <alexander.brown@a4direct.com>
2015-11-09 22:12:15 +0000

    Add twelveth unit test - refactor

--- MathAssistant.Core/NumericTransformer.cs
+++ MathAssistant.Core/NumericTransformer.cs
@@ -1,18 +1,41 @@
-﻿namespace MathAssistant.Core
+﻿using System;
+
+namespace MathAssistant.Core
 {
     public class NumericTransformer : INumericTransformer
     {
         public string Transform(int number)
         {
-            string returnString = number.ToString(); ;
+            var returnString = String.Empty; 
 
-            if (IsFizz(number))
-                returnString = "Fizz";
-            if (number == 5)
+            returnString = FizzRule(number, returnString);
+            returnString = BuzzRule(number, returnString);
+            if (String.IsNullOrEmpty(returnString))
+            {
+                returnString = number.ToString();  
+            }
+            return returnString;
+        }
+
+        private static string BuzzRule(int number, string returnString)
+        {
+            if (IsBuzz(number))
             {
                 returnString = "Buzz";
             }
             return returnString;
+        }
+
+        private static string FizzRule(int number, string returnString)
+        {
+            if (IsFizz(number))
+                returnString = "Fizz";
+            return returnString;
+        }
+
+        private static bool IsBuzz(int number)
+        {
+            return number == 5;
         }
 
         private static bool IsFizz(int number)
