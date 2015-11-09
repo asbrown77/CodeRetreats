0f914868fc42c616bf774de925471cf78ae492fd
Alexander Brown <alexander.brown@a4direct.com>
2015-11-09 22:57:01 +0000

    Add unit test - pass (while) refactor

--- MathAssistant.Console/Program.cs
+++ MathAssistant.Console/Program.cs
@@ -1,10 +1,32 @@
 ﻿
+using MathAssistant.Core;
+using MathAssistant.Core.Interfaces;
+using MathAssistant.Core.TransformRules;
+
 namespace MathAssistant.Console
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            var console = new ConsoleWrapper();
+            var numericTransformer = new NumericTransformer(new FizzBuzzRule());
+            var application = new MathAssistantApplication(console, numericTransformer);
+
+            application.Start();
+        }
+    }
+
+    public class ConsoleWrapper : IConsole
+    {
+        public void WriteLine(string value)
+        {
+            System.Console.WriteLine(value);
+        }
+
+        public string ReadLine()
+        {
+            return System.Console.ReadLine();
         }
     }
 }
