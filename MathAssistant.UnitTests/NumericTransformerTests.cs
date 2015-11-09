8b4c9d9e85d1040eadab5dbf2386fc4a7efe3103
Alexander Brown <alexander.brown@a4direct.com>
2015-11-09 17:32:31 +0000

    Add eighth unit test - fail

---
+++ MathAssistant.UnitTests/NumericTransformerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MathAssistant.UnitTests
+{
+    [TestFixture]
+    public class NumericTransformerTests
+    {
+        [Test]
+        public void Transform_WhenOne_ReturnStringOne()
+        {
+            var transformer = new NumericTransformer();
+            var result = transformer.Transform(1);
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+
+    public class NumericTransformer
+    {
+        public string Transform(int number)
+        {
+            return null;
+        }
+    }
+}
