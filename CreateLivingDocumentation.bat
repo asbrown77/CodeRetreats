f929106c580f21a38196755a6b46162c62767684
Alexander Brown <alexander.brown@a4direct.com>
2017-12-07 12:43:45 +0000

    Folder change from document but to specification

--- CreateLivingDocumentation.bat
+++ CreateLivingDocumentation.bat
@@ -1,6 +1,6 @@
 .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /labels /out=TestResult.txt /xml=TestResult.xml MathAssistant.AcceptanceTests\bin\debug\MathAssistant.AcceptanceTests.dll
 
-.\packages\Pickles.CommandLine.2.17.0\tools\pickles.exe --f=MathAssistant.AcceptanceTests\Features^ -o=.\document\ --lr=.\TestResult.xml --df=dhtml
+.\packages\Pickles.CommandLine.2.17.0\tools\pickles.exe --f=MathAssistant.AcceptanceTests\Features^ -o=.\specification\ --lr=.\TestResult.xml --df=dhtml
 
 pause
 
